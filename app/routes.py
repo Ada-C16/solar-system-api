ffd12c0e6eeb0c2bfbc7ca7c8cbf5605edd838a6
Rebecca Z <80494343+rzuick@users.noreply.github.com>
2021-10-15 12:56:26 -0500

    created Planet class and instances of Planet class

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,15 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description, distance):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.distance = distance
+
+planets = [
+    Planet(1, "Mars", "The red planet", "Inner part of solar system"),
+    Planet(2, "Jupiter", "The giant planet", "Outer part of the solar system"),
+    Planet(3, "Pluto", "The no-longer a planet, planet", "The outer, outer limits"),
+    Planet(4, "Venus", "The gassy one", "Inner planet, Earth's neighbor")
+]
