aa0759e22bcc955f01a0ded2fe8b8f769e02d9d0
asya0107 <asyasmith15@gmail.com>
2021-10-15 14:03:29 -0400

    planets and instances

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,14 @@
 from flask import Blueprint
 
+
+class Planet:
+    def __init__(self, id, name, description, color):
+        self.planet_id = id
+        self.name = name
+        self.description = description
+        self.color = color
+    
+
+planets = [Planet(1, 'Mars', '3rd planet from the sun', 'Red'), Planet(2, 'Jupiter', 'the largest planet in our solar system', 'Blue'), Planet(3, 'Venus', '2nd closest to the sun', 'Pink')]
+        
+
