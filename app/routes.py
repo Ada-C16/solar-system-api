17490c1a67d93bd0ec4214fa66390ff341bef9a7
kaitlyngore <kaitlyngore@gmail.com>
2021-10-15 14:09:33 -0400

    added planet class and planet list

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from planet import Planet
 
+planets = [
+    Planet(1, "Mercury", "Mercury is the smallest planet in the Solar System and the closest to the Sun", "terrestrial"), 
+    Planet(2, "Venus", "Venus is the second planet from the Sun.", "terrestrial")
+]
+
+planet_list_bp = Blueprint("planet_list_bp", __name__, url_prefix="/planets")
+
+@planet_list_bp.route("", methods=["GET"])
+def handle_planets():
+    planet_response = []
+    for planet in planets:
+        planet_response.append(
+            {"id:" planet.id,
+            "name": planet.name,
+            "description": planet.description},
+            "type": planet.type
+            })
+    
+    return jsonify(planet_response)
