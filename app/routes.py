be4bc0a875b8627a6ee1c235210d31a9d2316559
Rebecca Z <80494343+rzuick@users.noreply.github.com>
2021-10-15 13:15:40 -0500

    created endpoint to get one planet specifically

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet:
     def __init__(self, id, name, description, distance):
@@ -13,3 +13,33 @@ planets = [
     Planet(3, "Pluto", "The no-longer a planet, planet", "The outer, outer limits"),
     Planet(4, "Venus", "The gassy one", "Inner planet, Earth's neighbor")
 ]
+
+planets_bp = Blueprint("/planets", __name__, url_prefix="/planets")
+@planets_bp.route("", methods=["GET"])
+def get_planets():
+    planets_response = []
+    for planet in planets:
+        response = {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "distance": planet.distance,
+            "status": 200
+        }
+        planets_response.append(response)
+    return jsonify(planets_response)
+
+
+@planets_bp.route("/<planet_id>", methods = ["GET"])
+def get_one_planet(planet_id):
+    planet_id = int(planet_id)
+    for planet in planets:
+        if planet.id == planet_id:
+            response = {
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "distance": planet.distance,
+                "status": 200
+            }
+    return jsonify(response)
