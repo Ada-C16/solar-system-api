5689c20abb129c0ed2d20caceed8ee53eb5c4fba
Vange Spracklin <houseofvange@gmail.com>
2021-10-15 11:13:01 -0700

    reads all planets

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description, sign):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.sign = sign
+
+venus = Planet(1, "Venus", "Planet of beauty", "Libra")
+mercury = Planet(2, "Mercury", "Planet of very compute", "Virgo")
+jupiter = Planet(3, "Jupiter", "Planet of luck", "Saggitarius")
+    
+planets = [venus, mercury, jupiter]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods = ["GET"])
+def get_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "sign": planet.sign
+        })
+    return jsonify(planets_response)
