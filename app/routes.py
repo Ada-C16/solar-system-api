e12e945e173a33b7c19dfafd2952ce9ec1d74112
Reid Maute <reidhowdy@gmail.com>
2021-10-15 11:01:37 -0700

    First part done

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, name, description, color):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.color = color
+
+planets = [
+    Planet(1, "Venus", "A planet that spins in the opposite direction", "orange"),
+    Planet(2, "Mars", "Dusty, Cold, Desered", "red"),
+    Planet(3, "Mercury", "The smallest planet", "many colors"),
+]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def handle_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "color": planet.color
+        })
+    return jsonify(planets_response)
