5f09c26822945fd1f9f596654c49ae9a3dd0c4a7
Melinda <melindahayes84@gmail.com>
2021-10-18 14:23:13 -0700

    created planet class and a list of planet instances

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, jasonify
+
+class Planet:
+    def __init__(self, id , name , description , moons):
+        self.id = id 
+        self.name = name
+        self.description = description
+        self.moons = moons
+
+planets = [
+    Planet(1,"earth","our world",["Moon"]),
+    Planet(2,"mars","The red planet",["Phobos","Deimos"]),
+    Planet(3,"jupiter", "The biggest one",["Lo","Europa","Callisto","Gayemede"])
+    
+]
 
