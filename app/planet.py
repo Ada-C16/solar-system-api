8b4e44c6c5286f9e5e052028e03cdb3cd5029e87
Lux Barker <lux.barker13@gmail.com>
2021-10-18 13:23:41 -0700

    create Planet class

---
+++ app/planet.py
@@ -0,0 +1,6 @@
+class Planet:
+    def __init__(self, id, name, description, moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.moons = moons
