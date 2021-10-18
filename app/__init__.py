9e5c8f1e3db0644a052a6161c161c79ec1e7b59f
asya0107 <asyasmith15@gmail.com>
2021-10-18 17:15:14 -0400

    finished product of solar systems API

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
+
+
+
+
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app(test_config = None):
     app = Flask(__name__)
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,40 @@
-from flask import Blueprint
+
+
+
+
+
+
+
+
+
+from flask import Blueprint, jsonify
+
+planets_bp = Blueprint('planets', __name__, url_prefix = '/planets')
 
 
 class Planet:
     def __init__(self, id, name, description, color):
-        self.planet_id = id
+        self.id = id
         self.name = name
         self.description = description
         self.color = color
     
 
 planets = [Planet(1, 'Mars', '3rd planet from the sun', 'Red'), Planet(2, 'Jupiter', 'the largest planet in our solar system', 'Blue'), Planet(3, 'Venus', '2nd closest to the sun', 'Pink')]
-        
 
+
+@planets_bp.route('', methods = ['GET'])       
+def get_all_planets():
+    all_planets = []
+    for planet in planets:
+        all_planets.append({'id': planet.id,'name': planet.name, 'description':planet.description, 'color': planet.color} )
+    return jsonify(all_planets)
+
+
+@planets_bp.route('/<planet_name>', methods = ['GET'])
+def get_specific_planet(planet_name):
+    for planet in planets:
+        if planet.name == planet_name:
+            return {'id': planet.id,'name': planet.name, 'description':planet.description, 'color': planet.color}
+        
+    return 'Planet not found'
