db2cfa9b1a584d3ca8e8df5f41675d388c3d6c1f
Mac <mackenziespeyton@gmail.com>
2021-10-18 14:44:57 -0700

    Created an end point to read all planets

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jasonify
+from flask import Blueprint, jsonify
 
 class Planet:
     def __init__(self, id , name , description , moons):
@@ -11,6 +11,18 @@ planets = [
     Planet(1,"earth","our world",["Moon"]),
     Planet(2,"mars","The red planet",["Phobos","Deimos"]),
     Planet(3,"jupiter", "The biggest one",["Lo","Europa","Callisto","Gayemede"])
-    
 ]
 
+planets_bp = Blueprint("planet", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def get_all_planets():
+    planets_response = []
+    for planet in planets: 
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name, 
+            "description": planet.description, 
+            "moons": planet.moons
+        })
+    return jsonify(planets_response)
