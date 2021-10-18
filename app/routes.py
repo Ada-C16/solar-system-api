d874a87d928341172cb6016fb4bc85d22635592f
Alma <abecerrilsalas@gmail.com>
2021-10-18 14:14:46 -0700

    create endpoints for requesting all planets and one existing planet

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet:
     def __init__(self, id, name, description, color):
@@ -8,7 +8,35 @@ class Planet:
         self.color=color
 
 planets=[
-    Planet(1,"Sun", "center of the solar system", "orange"),
+    Planet(1, "Sun", "center of the solar system", "orange"),
     Planet(2, "Saturn", "takes approx 29.5 years to complete its orbit", "yellow"),
     Planet(3, "Mercury", "rules communication", "light grey")
 ]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"])
+def handle_planets():
+    planets_response = []
+    for planet in planets:
+        planets_response.append(
+            {
+                "id":planet.id,
+                "name":planet.name,
+                "description":planet.description,
+                "color":planet.color
+            }
+        )
+    return jsonify(planets_response)
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet_id=int(planet_id)
+    for planet in planets:
+        if planet.id == planet_id:
+            return {
+                "id":planet.id,
+                "name":planet.name,
+                "description":planet.description,
+                "color":planet.color
+            },200
