688dabf71a2998f2c6d98c81fa3c13da1039f545
sjolivas <sjolivas@gmail.com>
2021-10-18 12:34:22 -0500

    Added more planet instances

--- app/routes.py
+++ app/routes.py
@@ -8,10 +8,15 @@ class Planet:
         self.distance = distance
 
 planets = [
-    Planet(1, "Mars", "The red planet", "Inner part of solar system"),
-    Planet(2, "Jupiter", "The giant planet", "Outer part of the solar system"),
-    Planet(3, "Pluto", "The no-longer a planet, planet", "The outer, outer limits"),
-    Planet(4, "Venus", "The gassy one", "Inner planet, Earth's neighbor")
+    Planet(1, "Mercury", "Hot, but not too hot for ice", "Innermost planet, closest to the Sun"),
+    Planet(2, "Venus", "The gassy one", "Inner planet, Earth's neighbor"),
+    Planet(3, "Earth", "Blue and green rock where we exist", "Inner solar system, Goldilocks"),
+    Planet(4, "Mars", "The red planet", "Inner part of solar system"),
+    Planet(5, "Jupiter", "The giant planet", "Outer part of the solar system"),
+    Planet(6, "Saturn", "The one with all of the (most visible) rings", "Outter part of the solar system"), 
+    Planet(7, "Uranus", "The lazy one that rotates on it's side", "Outter part of the solar system"),
+    Planet(8, "Neptune", "Named after it's beautiful blue color, but not discovered by sight, instead by mathematical calculations", "Outter part of the solar syatem"),
+    Planet(9, "Pluto", "The no-longer a planet, planet", "The outer, outer limits")
 ]
 
 planets_bp = Blueprint("/planets", __name__, url_prefix="/planets")
