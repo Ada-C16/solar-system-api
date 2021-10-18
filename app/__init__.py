4c8696892d7e53c7a7a522b85d9408d45fa6c91c
Veronica Osei <veronicaosei273@gmail.com>
2021-10-18 15:23:40 -0500

    complete solar-system

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    from .routes import planet_system_bp
+    app.register_blueprint(planet_system_bp)
+
+    from .routes import planets_bp
+    app.register_blueprint(planets_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint,jsonify
+
+class Planet:
+    def __init__(self,id,name,description):
+        self.id = id
+        self.name = name
+        self.description = description
+
+planets = [
+    Planet(1, "Mercury", "Very windy."),
+    Planet(2, "Venus", "Brightest planet."),
+    Planet(3, "Saturn", "Icy rings.")
+]
+
+planet_system_bp = Blueprint("planet_system", __name__)
+planets_bp = Blueprint("planets_py", __name__, url_prefix= "/planets")
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def handle_planets(planet_id):
+
+    for planet in planets:
+        if planet.id == int(planet_id):
+            return {
+                "id" : planet.id,
+                "name" : planet.name,
+                "description" : planet.description
+            }
+
+
+@planet_system_bp.route("/planets", methods= ['GET'])
+def get_planet_system_json():
+    return {
+        "planet_1" : "Mercury",
+        "planet_2" : "Venus",
+        "planet_3" : "Saturn"
+    }
+
+
+
 
