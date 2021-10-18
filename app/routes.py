3d7bffd2dff311eb797760b52b2a61f81a76d0e2
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2021-10-18 14:27:07 -0700

    Adds get_planet function to routes.py

--- app/routes.py
+++ app/routes.py
@@ -11,17 +11,14 @@ class Planet():
 
 PLANETS = [
 
-    Planet(426, "Nostromo's End",
-           "Hostile weather.  Toxic atmosphere.  Evidence of civilization.", True),
+    Planet(426, "Nostromo's End", "Hostile weather.  Toxic atmosphere.  Evidence of civilization.", True),
     Planet(224, "JollyPlanet", "Okay. Decent. Will live for long time.", True)
 
 ]
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
-# @blueprint_name.route("", methods=["GET"])
-
-
+# @blueprint_name.route("/endpoint/path/here", methods=["GET"])
 @planets_bp.route("", methods=["GET"])
 def get_all_planets():
     planets_response = []
@@ -30,4 +27,13 @@ def get_all_planets():
         planets_response.append(vars(planet))
 
     return jsonify(planets_response)
+
+@planets_bp.route("/planet_id", methods=["GET"])
+def get_planet(planet_id):
+    planet_id = int(planet_id)
+
+    for planet in PLANETS:
+        if planet.id == planet_id:
+            return vars(planet_id)
+
 
