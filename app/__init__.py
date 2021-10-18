fd13777b275e326f88a8a159050c9f5365ae6c58
Tirhas Gebreyohannes <tirhastse@gmail.com>
2021-10-18 13:59:36 -0700

    Initial setup

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,7 @@ from flask import Flask
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    from .routes import solarsystem_bp
+    app.register_blueprint(solarsystem_bp)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,23 @@
 from flask import Blueprint
 
+solarsystem_bp = Blueprint("solarsystem", __name__, url_prefix="/solarsystem")
+
+class SolarSysten:
+    def __init__(self, name,description):
+        self.name = name
+        self.has_ring = True
+        self.description = description
+        
+ @solarsystem_bp.route("solarsystem", methods=["GET"])
+ 
+ planets = [
+     SolarSystem()
+ ]
+def handle_solarsystem():
+    solarsystem_response = []
+    for planet in solarsystem:
+        solarsystem_response.append({
+            
+        })
+    return jsonify(solarsystem_response)       
+        
