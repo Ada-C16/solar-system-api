d3c133b8d71e8dfd5b6483ca3d42f7b292fefa7f
Rebeca <muniz.movement@gmail.com>
2021-10-18 14:29:36 -0700

    Create planet class and instances.

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,15 @@
 from flask import Blueprint
+class Planet:
+    def __init__(self, id, name, description, moon):
+        self.id = id
+        self.name = name
+        self.description = description 
+        self.moon = moon 
 
+planets = [
+    Planet(1, "Saturn", "A gassy, heavy, giant who's sixth from the sun. Most likely compposed of iron, nickel, and rock.", 82),
+    Planet(2, "Mercury", "When it retrogrades everything in the world sucks.", 0),
+    Planet(3, "Venus", "It is named after the goddess of love and beauty. Second brightest object in the sky.", 0)
+]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
