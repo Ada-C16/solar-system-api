f02b6d012597faafa1ce8882312cf1b4e4556aa4
Khandice <s.khandice@gmail.com>
2021-10-18 13:40:56 -0700

    Created Planet class and registered blueprint with app

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+class Planet:
+    def __init__(self, id, name, description, matter):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.matter = matter
+
+
+planets = [Planet(1, "Mercury", "small and red", "solid"),     Planet(5, "Jupiter", "big and swirly", "gaseous"), 
+Planet(6, "Saturn", "rings and swirls", "gaseous")]
+
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
