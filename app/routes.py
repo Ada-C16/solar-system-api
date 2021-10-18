8f151f5e816766f93c786beae27d5275d5386267
Michelle Bodart <michelle.bodart@gmail.com>
2021-10-18 13:47:41 -0700

    Created Planet class, created planets blueprint and registered it in __init__, made a list of 3 planets

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,18 @@
 from flask import Blueprint
 
+class Planet():
+
+    def __init__(self, id, name, description, major_moons=None):
+        self.id = id
+        self.name = name
+        self.description = description
+        if major_moons == None:
+            self.major_moons = []
+        else:
+            self.major_moons = major_moons
+
+PLANETS = [
+    Planet(1, "Mercury", "The smallest planet in our solar system", None),
+    Planet(2, "Saturn", "Saturn is a gas giant made of mostly hydrogen and helium", ["Titan", "Dione", "Enceladus", "Hyperion"]),
+    Planet(3, "Pluto", "Not sure if it's a real planet, but we like it", ["Charon"])
+]
