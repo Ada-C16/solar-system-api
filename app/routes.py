eb71eb602ed8163d5bcb584bacda7514d2ab1f5d
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2021-10-18 14:14:27 -0700

    Add get all functions and registers blueprint class

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 
 class Planet():
-    def __init__(self, id, name, description, xenomorphs=None):
+    def __init__(self, id, name, description, xenomorphs=False):
         self.id = id
         self.name = name
         self.description = description
@@ -11,4 +11,23 @@ class Planet():
 
 PLANETS = [
 
+    Planet(426, "Nostromo's End",
+           "Hostile weather.  Toxic atmosphere.  Evidence of civilization.", True),
+    Planet(224, "JollyPlanet", "Okay. Decent. Will live for long time.", True)
+
 ]
+
+planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
+# @blueprint_name.route("", methods=["GET"])
+
+
+@planets_bp.route("", methods=["GET"])
+def get_all_planets():
+    planets_response = []
+
+    for planet in PLANETS:
+        planets_response.append(vars(planet))
+
+    return jsonify(planets_response)
+
