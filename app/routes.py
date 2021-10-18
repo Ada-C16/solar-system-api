c5a9bfc777fb8f3b9b50c3f98ced24f5db83daf6
KitSutliff <38538497+KitSutliff@users.noreply.github.com>
2021-10-18 13:42:48 -0700

    Adds class Planet to routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,14 @@
 from flask import Blueprint
 
+
+class Planet():
+    def __init__(self, id, name, description, xenomorphs=None):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.xenomorphs = xenomorphs
+
+
+PLANETS = [
+
+]
