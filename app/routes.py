97a70245fb3728183c2b50247dd6e75f34496c9f
Roslyn Melookaran <rmelookaran@gmail.com>
2021-10-18 13:30:00 -0700

    Added Planet class and list of planet instances

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,21 @@
 from flask import Blueprint, jsonify
 
+class Planet:
+    def __init__(self, id, title, description, moon):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.moon = moon
+
+planets= [
+    Planet(1, "Mercury", "Diameter: 3,031 miles (4,878 km)", False), 
+    Planet(2, "Venus", "Diameter: 7,521 miles (12,104 km)", False),
+    Planet(3, "Earth", "Diameter: 7,926 miles (12,760 km)", True),
+    Planet(4, "Mars", "Diameter: 4,217 miles (6,787 km)", True), 
+    Planet(5, "Jupiter", "Diameter: 86,881 miles (139,822 km)", True), 
+    Planet(6, "Saturn", "Diameter: 74,900 miles (120,500 km)", True),
+    Planet(7, "Uranus", "Diameter: 31,763 miles (51,120 km)", True),
+    Planet(8, "Neptune", "Diameter: 30,775 miles (49,530 km)", True)
+]
+
 planets_bp = Blueprint("planets_bp", __name__,url_prefix="/planets")
