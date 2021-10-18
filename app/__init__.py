b48115bd1f0b84a589926be00e54b1c9d965e003
Kayla Huddleston <khuddleup@gmail.com>
2021-10-18 15:01:17 -0700

    Solar System Part 1: Complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
+#create app function and registering blueprints
 from flask import Flask
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    from .routes import planets_bp #importing from routes
+    app.register_blueprint(planets_bp) #registering the route
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,42 @@
-from flask import Blueprint
+#define our class and make bluprints
+from flask import Blueprint, jsonify
 
+#class are capitalized
+class Planet: 
+    def __init__(self, id, name, description, num_moons):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.num_moons = num_moons
+
+planets = [
+    Planet(1, "Earth", "Green and Blue", 1), 
+    Planet(2, "Mars", "Red and Orange", 2),
+    Planet(3, "Jupiter", "Gray and Brown", 79)
+]
+
+planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["GET"]) #decorator communicates with flask
+def get_planets(): #function does the work
+    planets_response = []
+    for planet in planets:
+        planets_response.append({
+            "id" : planet.id,
+            "name" : planet.name,
+            "description" : planet.description,
+            "num_moons" : planet.num_moons
+        })
+    return jsonify(planets_response)
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_planet(planet_id):
+    for planet in planets:
+        if planet.id == int(planet_id):
+            response = {
+            "id" : planet.id,
+            "name" : planet.name,
+            "description" : planet.description,
+            "num_moons" : planet.num_moons
+        }
+            return jsonify(response)
