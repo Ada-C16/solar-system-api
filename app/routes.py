53c37bd4cac1db2e798ebd4e8ab10ec14e41f3df
Alma <abecerrilsalas@gmail.com>
2021-10-18 13:40:03 -0700

    Co-authored-by: tt-ht <tt-ht@users.noreply.github.com>

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,14 @@
 from flask import Blueprint
 
+class Planet:
+    def __init__(self, id, name, description, color):
+        self.id=id
+        self.name=name
+        self.description=description
+        self.color=color
+
+planets=[
+    Planet(1,"Sun", "center of the solar system", "orange"),
+    Planet(2, "Saturn", "it takes approx 29.5 years to complete its orbit","yellow"),
+    Planet(3, "Mercury", "rules communication", "light grey")
+]
