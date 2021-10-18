185961ee6d60f541d854dd049a310c64ce209b82
Lux Barker <lux.barker13@gmail.com>
2021-10-18 13:59:10 -0700

    create endpoint for getting one planet

--- app/routes.py
+++ app/routes.py
@@ -5,16 +5,26 @@ from .planet import Planet
 planets = [
     Planet(1, "Mars", "Red Planet", ["Phobos", "Deimos"]),
     Planet(2, "Earth", "Blue Marbel", ["Luna"]),
-    Planet(3, "Pluto", "It IS a planet!", ["Charon", "Nix", "Hydra", "Kerberos", "Styx"]),
+    Planet(3, "Pluto", "It IS a planet!", [
+           "Charon", "Nix", "Hydra", "Kerberos", "Styx"]),
     Planet(4, "Venus", "She's Pretty", [])
 ]
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@planets_bp.route("", methods = ["GET"])
+
+@planets_bp.route("", methods=["GET"])
 def handle_planets():
     response = []
     for planet in planets:
         response.append(planet.to_dict())
 
     return jsonify(response)
+
+
+@planets_bp.route("<id>", methods=["GET"])
+def handle_planet(id):
+    id = int(id)
+    for planet in planets:
+        if id == planet.id:
+            return planet.to_dict()
