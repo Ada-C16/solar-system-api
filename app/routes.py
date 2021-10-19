01764a7ec78455a4594760e5db2cd9a7c98e4bd6
sarahstandish <z.sisyphusrocks@gmail.com>
2021-10-19 11:31:29 -0700

    add psuedocode

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,10 @@
 from flask import Blueprint
 
+# create blueprint which is the decorator of the routes
+# it will have a url prefix of /planets
+# endpoint will have a prefix of an empty strings
+# methods will be get
+# create the function for handle planets
+# empty list
+# loop through planets and add to list
+# return the list in json
