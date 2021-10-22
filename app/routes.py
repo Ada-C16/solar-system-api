3fc45e458ff5b35f3d1d5aa96bee20f5ee2f0722
sjolivas <sjolivas@gmail.com>
2021-10-22 12:43:05 -0500

    Initialized changes to routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,25 @@
-from flask import Blueprint, jsonify
-import requests
+from flask import Blueprint, jsonify, make_response, request
+from app import db
+from app.models.planet import Planet
 
-class Planet:
-    def __init__(self, id, name, description, distance):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.distance = distance
+# class Planet:
+#     def __init__(self, id, name, description, distance):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.distance = distance
 
-planets = [
-    Planet(1, "Mercury", "Hot, but not too hot for ice", "Innermost planet, closest to the Sun"),
-    Planet(2, "Venus", "The gassy one", "Inner planet, Earth's neighbor"),
-    Planet(3, "Earth", "Blue and green rock where we exist", "Inner solar system, Goldilocks"),
-    Planet(4, "Mars", "The red planet", "Inner part of solar system"),
-    Planet(5, "Jupiter", "The giant planet", "Outer part of the solar system"),
-    Planet(6, "Saturn", "The one with all of the (most visible) rings", "Outter part of the solar system"), 
-    Planet(7, "Uranus", "The lazy one that rotates on it's side", "Outter part of the solar system"),
-    Planet(8, "Neptune", "Named after it's beautiful blue color, but not discovered by sight, instead by mathematical calculations", "Outter part of the solar syatem"),
-    Planet(9, "Pluto", "The no-longer a planet, planet", "The outer, outer limits")
-]
+# planets = [
+#     Planet(1, "Mercury", "Hot, but not too hot for ice", "Innermost planet, closest to the Sun"),
+#     Planet(2, "Venus", "The gassy one", "Inner planet, Earth's neighbor"),
+#     Planet(3, "Earth", "Blue and green rock where we exist", "Inner solar system, Goldilocks"),
+#     Planet(4, "Mars", "The red planet", "Inner part of solar system"),
+#     Planet(5, "Jupiter", "The giant planet", "Outer part of the solar system"),
+#     Planet(6, "Saturn", "The one with all of the (most visible) rings", "Outter part of the solar system"), 
+#     Planet(7, "Uranus", "The lazy one that rotates on it's side", "Outter part of the solar system"),
+#     Planet(8, "Neptune", "Named after it's beautiful blue color, but not discovered by sight, instead by mathematical calculations", "Outter part of the solar syatem"),
+#     Planet(9, "Pluto", "The no-longer a planet, planet", "The outer, outer limits")
+# ]
 
 planets_bp = Blueprint("/planets", __name__, url_prefix="/planets")
 @planets_bp.route("", methods=["GET"])
