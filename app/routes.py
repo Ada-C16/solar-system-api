3744ed84c0de203938b9e063c9fcb77348258d43
Vange Spracklin <houseofvange@gmail.com>
2021-10-22 11:15:31 -0700

    adds Planet model

--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,43 @@
 from flask import Blueprint, jsonify
 
-class Planet:
-    def __init__(self, id, name, description, sign):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.sign = sign
-
-venus = Planet(1, "Venus", "Planet of beauty", "Libra")
-mercury = Planet(2, "Mercury", "Planet of very compute", "Virgo")
-jupiter = Planet(3, "Jupiter", "Planet of luck", "Saggitarius")
-    
-planets = [venus, mercury, jupiter]
-
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@planets_bp.route("", methods = ["GET"])
-def get_planets():
-    planets_response = []
-    for planet in planets:
-        planets_response.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "sign": planet.sign
-        })
-    return jsonify(planets_response)
+# class Planet:
+#     def __init__(self, id, name, description, sign):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.sign = sign
 
-@planets_bp.route("/<planet_name>", methods = ["GET"])
-def get_planet(planet_name):
-    for planet in planets:
-        if planet.name.lower() == planet_name.lower():
-            planet_response = {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "sign": planet.sign
-            }
-    return planet_response
+
+
+# YE OLDE STUFF
+# venus = Planet(1, "Venus", "Planet of beauty", "Libra")
+# mercury = Planet(2, "Mercury", "Planet of very compute", "Virgo")
+# jupiter = Planet(3, "Jupiter", "Planet of luck", "Saggitarius")
+    
+# planets = [venus, mercury, jupiter]
+
+# @planets_bp.route("", methods = ["GET"])
+# def get_planets():
+#     planets_response = []
+#     for planet in planets:
+#         planets_response.append({
+#             "id": planet.id,
+#             "name": planet.name,
+#             "description": planet.description,
+#             "sign": planet.sign
+#         })
+#     return jsonify(planets_response)
+
+# @planets_bp.route("/<planet_name>", methods = ["GET"])
+# def get_planet(planet_name):
+#     for planet in planets:
+#         if planet.name.lower() == planet_name.lower():
+#             planet_response = {
+#                 "id": planet.id,
+#                 "name": planet.name,
+#                 "description": planet.description,
+#                 "sign": planet.sign
+#             }
+#     return planet_response
