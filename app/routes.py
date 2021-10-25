aedf5d83b63510246d5fb1e1528bd0db88119549
Alma <abecerrilsalas@gmail.com>
2021-10-25 14:49:51 -0700

    Refactors to create one planet or read all planets

--- app/routes.py
+++ app/routes.py
@@ -4,25 +4,39 @@ from flask import Blueprint, jsonify, make_response, request
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-# Create a planet
-@planets_bp.route("", methods=["POST"])
+# Endpoint to create one planet or read all planets
+@planets_bp.route("", methods=["POST", "GET"])
 def create_planet():
-    request_body = request.get_json()
+    if request.method == "POST":
+        request_body = request.get_json()
 
-    if "name" not in request_body or "description" not in request_body or "color" not in request_body:
-        return make_response("Invalid request", 400)
+        if "name" not in request_body or "description" not in request_body or "color" not in request_body:
+            return make_response("Invalid request", 400)
 
-    new_planet = Planet(
-        name=request_body["name"],
-        description=request_body["description"],
-        color=request_body["color"]
-    )
+        new_planet = Planet(
+            name=request_body["name"],
+            description=request_body["description"],
+            color=request_body["color"]
+        )
 
-    db.session.add(new_planet)
-    db.session.commit()
+        db.session.add(new_planet)
+        db.session.commit()
 
-    return f"Planet {new_planet.name} created", 201
+        return f"Planet {new_planet.name} created", 201
 
+    elif request.method == "GET":
+        planets=Planet.query.all()
+        planets_response=[]
+        for planet in planets:
+            planets_response.append(
+                {
+                    "id": planet.id,
+                    "name": planet.name,
+                    "description": planet.description,
+                    "color": planet.color
+                }
+            )
+        return jsonify(planets_response)
 
 # @planets_bp.route("", methods=["GET"])
 # def handle_planets():
