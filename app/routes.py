7a79290acd0b2e3b0349988337ffed96d6a49dca
Mariah Rosman <mrosman7@gmail.com>
2021-10-25 14:34:55 -0700

    refactors routes.py to query data from the solar_system_development database and refactors get_planets and get_planet routes accordingly

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,30 @@
 #define our class and make bluprints
 from flask import Blueprint, jsonify
+from app.models.planets import Planet
+from app import db
 
 #class are capitalized
-class Planet: 
-    def __init__(self, id, name, description, num_moons):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.num_moons = num_moons
+# class Planet: 
+#     def __init__(self, id, name, description, num_moons):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.num_moons = num_moons
 
-planets = [
-    Planet(1, "Earth", "Green and Blue", 1), 
-    Planet(2, "Mars", "Red and Orange", 2),
-    Planet(3, "Jupiter", "Gray and Brown", 79)
-]
+# planets = [
+#     Planet(1, "Earth", "Green and Blue", 1), 
+#     Planet(2, "Mars", "Red and Orange", 2),
+#     Planet(3, "Jupiter", "Gray and Brown", 79)
+# ]
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
 
 @planets_bp.route("", methods=["GET"]) #decorator communicates with flask
 def get_planets(): #function does the work
     planets_response = []
+    planets = Planet.query.all()
+
     for planet in planets:
         planets_response.append({
             "id" : planet.id,
@@ -27,18 +32,19 @@ def get_planets(): #function does the work
             "description" : planet.description,
             "num_moons" : planet.num_moons
         })
-    return jsonify(planets_response)
+    return jsonify(planets_response), 200
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def get_planet(planet_id):
-    for planet in planets:
-        if planet.id == int(planet_id):
-            response = {
+    planet_id = int(planet_id)
+    planet = Planet.query.all(planet_id)
+    
+    if planet:
+        return {
             "id" : planet.id,
             "name" : planet.name,
             "description" : planet.description,
             "num_moons" : planet.num_moons
-        }
-            return jsonify(response)
+            }, 200
     
-    return 'Planet ID not found', 404
+    return f'Planet {planet_id} not found', 404
