6054410538dea369553e350f746c26ab5e7a544d
Makhabat <maksatbekova.m@gmail.com>
2021-10-25 15:57:23 -0700

    Read one planet

--- app/routes.py
+++ app/routes.py
@@ -31,10 +31,22 @@ from flask import Blueprint, jsonify, make_response, request
 solar_systems_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 
-@solar_systems_bp.route("", methods=["POST"])
+@solar_systems_bp.route("", methods=["GET", "POST"])
 def handle_planets():
-    request_body = request.get_json()
-    new_planet = Planet(name=request_body["name"],
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "color": planet.color,
+            })
+        return jsonify(planets_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
                         description=request_body["description"],
                         color=request_body["color"])
 
@@ -42,6 +54,17 @@ def handle_planets():
     db.session.commit()
 
     return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+
+@solar_systems_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+
+    return {
+        "id":planet.id,
+        "name": planet.name,
+        "description": planet.description
+    }
 # @solar_systems_bp.route("", methods=["GET", "POST"])
 # def get_planet_json():
 #     planet_response = []
