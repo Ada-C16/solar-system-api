28019064e07a9c51b5c12d7573e63e22c5a6dc69
Arianna Cabebe <aricabebe@gmail.com>
2021-10-25 14:33:33 -0700

    adds POST endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,34 +1,41 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from app.models.planet import Planet
+from app import db
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-# class Planet:
-#     def __init__(self, id, name, description, sign):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.sign = sign
 
+@planets_bp.route("", methods = ["GET", "POST"])
+def get_planets():
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description,
+                "sign": planet.sign
+            })
+        if not planets_response:
+            return jsonify("There are no planets yet! :O", 200)
+        return jsonify(planets_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        if "name" not in request_body or "description" not in request_body or "sign" not in request_body:
+            return jsonify("Invalid request", 400)
+        
+        new_planet = Planet(
+            name = request_body["name"],
+            description = request_body["description"],
+            sign = request_body["sign"]
+        )
+        
+        db.session.add(new_planet)
+        db.session.commit()
 
+        return f"Planet {new_planet.name} created.", 201
 
-# YE OLDE STUFF
-# venus = Planet(1, "Venus", "Planet of beauty", "Libra")
-# mercury = Planet(2, "Mercury", "Planet of very compute", "Virgo")
-# jupiter = Planet(3, "Jupiter", "Planet of luck", "Saggitarius")
-    
-# planets = [venus, mercury, jupiter]
-
-# @planets_bp.route("", methods = ["GET"])
-# def get_planets():
-#     planets_response = []
-#     for planet in planets:
-#         planets_response.append({
-#             "id": planet.id,
-#             "name": planet.name,
-#             "description": planet.description,
-#             "sign": planet.sign
-#         })
-#     return jsonify(planets_response)
 
 # @planets_bp.route("/<planet_name>", methods = ["GET"])
 # def get_planet(planet_name):
