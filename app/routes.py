5f14bab176c485e338dc4e9cd574a80e81f17b88
bailey <baileyholderman@gmail.com>
2021-10-25 16:36:53 -0700

    refactors read all planets to handle a name parameter

--- app/routes.py
+++ app/routes.py
@@ -21,7 +21,12 @@ def get_planet_from_id(planet_id):
 @planets_bp.route("", methods = ["GET"])
 def read_all_planets():
 
-    planets = Planet.query.all()
+    name_query = request.args.get("name")
+
+    if name_query:
+        planets = Planet.query.filter_by(name=name_query)
+    else:
+        planets = Planet.query.all()
     
     planets_response = [planet.to_dict() for planet in planets]
 
