d9dc1edd997c295b417e5e40e7cf6f627447cedd
Lux Barker <lux.barker13@gmail.com>
2021-10-25 14:47:55 -0700

    refactor get one planet method

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,6 @@ from app.models.planet import Planet
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-# create new planet
-
 
 @planets_bp.route("", methods=["POST", "GET"])
 def handle_planets():
@@ -28,18 +26,11 @@ def handle_planets():
             planets_response.append(planet.to_dict())
         return jsonify(planets_response)
 
-# @planets_bp.route("", methods=["GET"])
-# def handle_planets():
-#     response = []
-#     for planet in planets:
-#         response.append(planet.to_dict())
 
-#     return jsonify(response)
-
-
-# @planets_bp.route("<id>", methods=["GET"])
-# def handle_planet(id):
-#     id = int(id)
-#     for planet in planets:
-#         if id == planet.id:
-#             return planet.to_dict()
+@planets_bp.route("<id>", methods=["GET"])
+def handle_planet(id):
+    try:
+        planet = Planet.query.get(id)
+    except:
+        return make_response("Invalid ID", 400)
+    return planet.to_dict()
