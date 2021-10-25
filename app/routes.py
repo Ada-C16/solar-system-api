b05d66d36daa988ac3e43c45b63a7be01b0fa066
Lux Barker <lux.barker13@gmail.com>
2021-10-25 13:55:19 -0700

    add to_dict method

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,6 @@
 from flask import Blueprint, jsonify
 
-from .planet import Planet
-
-planets = [
-    Planet(1, "Mars", "Red Planet", ["Phobos", "Deimos"]),
-    Planet(2, "Earth", "Blue Marbel", ["Luna"]),
-    Planet(3, "Pluto", "It IS a planet!", ["Charon", "Nix", "Hydra", "Kerberos", "Styx"]),
-    Planet(4, "Venus", "She's Pretty", [])
-]
+from app.models.planet import Planet
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
