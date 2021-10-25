653f4ea76707a35c73b4dde6d4700ebc1186c12b
Alma <abecerrilsalas@gmail.com>
2021-10-25 15:03:24 -0700

    Created endpoint to read one planet

--- app/routes.py
+++ app/routes.py
@@ -37,42 +37,19 @@ def create_planet():
                 }
             )
         return jsonify(planets_response)
+        
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = Planet.query.get(planet_id)
 
-# @planets_bp.route("", methods=["GET"])
-# def handle_planets():
-#     planets_response = []
-#     for planet in planets:
-#         planets_response.append(
-#             {
-#                 "id":planet.id,
-#                 "name":planet.name,
-#                 "description":planet.description,
-#                 "color":planet.color
-#             }
-#         )
-#     return jsonify(planets_response)
+    if planet is None:
+        return make_response(
+            f"Planet {planet_id} not found", 404
+        )
 
-# @planets_bp.route("/<planet_id>", methods=["GET"])
-# def handle_planet(planet_id):
-#     planet_id=int(planet_id)
-#     for planet in planets:
-#         if planet.id == planet_id:
-#             return {
-#                 "id":planet.id,
-#                 "name":planet.name,
-#                 "description":planet.description,
-#                 "color":planet.color
-#             },200
-
-# class Planet:
-#     def __init__(self, id, name, description, color):
-#         self.id=id
-#         self.name=name
-#         self.description=description
-#         self.color=color
-
-# planets=[
-#     Planet(1, "Sun", "center of the solar system", "orange"),
-#     Planet(2, "Saturn", "takes approx 29.5 years to complete its orbit", "yellow"),
-#     Planet(3, "Mercury", "rules communication", "light grey")
-# ]
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "color": planet.color
+    }, 200
