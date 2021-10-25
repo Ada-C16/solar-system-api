8472b16c510233273d735448a63d66d7b3c8be88
Sophia Le <sophiale@sophias-mbp.lan>
2021-10-25 14:36:14 -0700

    Add POST route for planets

--- app/__init__.py
+++ app/__init__.py
@@ -15,8 +15,9 @@ def create_app(test_config=None):
     db.init_app(app)
     migrate.init_app(app, db)
     
+    from app.models.planet import Planet
     from .routes import planet_bp
     app.register_blueprint(planet_bp)
-    from app.models.planet import Planet
+    
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,34 @@
-from flask import Blueprint, jsonify
-
-# class Planet:
-#     def __init__(self, id, name, description, color):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.color = color
-
-#     def to_dict(self):
-#         return {
-#             'id': self.id,
-#             'name': self.name,
-#             'description': self.description,
-#             'color': self.color
-#             }
-
-# planets = [
-#     Planet('4fr', 'Earth', 'Only known life planet', 'blue'),
-#     Planet('6rw', 'Neptune', 'The farthest known planet from the Sun', 'blue'),
-#     Planet('ty6', 'Saturn', 'Sixth planet from the Sun', 'yellow'),
-#     Planet('48t', 'Mars', 'Fourth planet from the Sun. Colonization in process', 'red')
-# ]
+from flask import Blueprint, jsonify, make_response, request
+from app import db
+from app.models.planet import Planet
 
 planet_bp = Blueprint('planets', __name__, url_prefix='/planets')
 
-@planet_bp.route('', methods=['GET'])
+@planet_bp.route('', methods=['POST'])
 def handle_planets():
-    planets_response = list()
+    request_body = request.get_json()
+    planet_to_add = Planet(
+        name=request_body['name'],
+        description=request_body['description'],
+        color=request_body['color'])
+    
+    db.session.add(planet_to_add)
+    db.session.commit()
 
-    for planet in planets:
-        planets_response.append(planet.to_dict())
-    return jsonify(planets_response)
+    return make_response(f'Planet {planet_to_add.name} successfully created', 201)
 
-@planet_bp.route('/<id>', methods=['GET'])
-def handle_single_planet(id):
-    for planet in planets:
-        if planet.id == id:
-            return jsonify(planet.to_dict())
-    return 'Error: Planet ID not Found'    
+# @planet_bp.route('', methods=['GET'])
+# def handle_planets():
+#     planets_response = list()
+
+#     for planet in planets:
+#         planets_response.append(planet.to_dict())
+#     return jsonify(planets_response)
+
+# @planet_bp.route('/<id>', methods=['GET'])
+# def handle_single_planet(id):
+#     for planet in planets:
+#         if planet.id == id:
+#             return jsonify(planet.to_dict())
+#     return 'Error: Planet ID not Found'    
 
