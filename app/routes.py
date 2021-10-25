25e9121c367f39d9af34fbdcf719166ee857d9d1
Alie Ibarra <alieibarra@gmail.com>
2021-10-25 13:45:57 -0700

    Add Models

--- app/routes.py
+++ app/routes.py
@@ -5,8 +5,7 @@ from .planet import Planet
 planets = [
     Planet(1, "Mars", "Red Planet", ["Phobos", "Deimos"]),
     Planet(2, "Earth", "Blue Marbel", ["Luna"]),
-    Planet(3, "Pluto", "It IS a planet!", [
-           "Charon", "Nix", "Hydra", "Kerberos", "Styx"]),
+    Planet(3, "Pluto", "It IS a planet!", ["Charon", "Nix", "Hydra", "Kerberos", "Styx"]),
     Planet(4, "Venus", "She's Pretty", [])
 ]
 
