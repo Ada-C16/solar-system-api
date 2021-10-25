d3187ca3b5c5cf6da7058feeb738c781e5ad11e2
goblinbrooke <BrookeVlahos@gmail.com>
2021-10-25 14:26:43 -0700

    GET and POST

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,46 @@
-from flask import Blueprint, jsonify
-from .list_of_planets import planets
+from flask import Blueprint, jsonify, request
+# from .list_of_planets import planets
 from app.models.planet import Planet
 from app import db 
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@planets_bp.route("", methods=["GET"])
+@planets_bp.route("", methods=["GET", "POST"])
 def handle_planets():
+    if request.method == "POST":
+        request_body = request.get_json()
+        if "name" not in request_body or "description" not in request_body or "has_moons" not in request_body:
+            return jsonify({"message": "Missing information - you need name, description, and if the planet has moons."}), 400
 
-    list_of_planets = []
-    for planet in planets:
-        list_of_planets.append(planet.create_planet_dictionary()), 200
+        new_planet = Planet(name = request_body["name"], description = request_body["description"], has_moons = request_body["has_moons"])
+
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return f"New Planet {new_planet.name} Added!", 201
+    elif request.method == "GET":
+        planets = Planet.query.all()
+        planet_list = []
+        for planet in planets:
+            planet_list.append(planet.create_planet_dictionary)
+        
+        return jsonify(planet_list), 200
+# Able to take both Get and Post requests
+# If POST: set up a variable to hold request body 
+# Use variable to create new planet instance
+# Add to .db session
+# Comit db session
+# Return a response with a 201 status code
+# If GET: Query database, get all planets
+# Create planets as dictionaries, put in a list
+# Return jsonify list
+
+
+    # list_of_planets = []
+    # for planet in planets:
+    #     list_of_planets.append(planet.create_planet_dictionary()), 200
     
-    return jsonify(list_of_planets)
+    # return jsonify(list_of_planets)
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def handle_planet(planet_id):
