cbe2c82a30583a1c235df491a9567e0a92a03e1d
Alma <abecerrilsalas@gmail.com>
2021-10-25 14:40:33 -0700

    Set up Planet model and migrations, create one planet endpoint Co-authored-by: tt-ht

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,30 @@
 from flask import Flask
+# Step 1:
+# Import and initialize SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+# Initialize SQLAlchemy
+db = SQLAlchemy()
+migrate = Migrate()
+DATABASE_CONNECTION_STRING='postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    # Step 2:
+    # Configure SQLAlchemy
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_STRING
+
+    # Import Models here!
+    from app.models.planet import Planet
+
+    # Step 3:
+    # Hook up Flask & SQLALchemy
+    db.init_app(app)
+    migrate.init_app(app, db)
+    
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
 
--- app/routes.py
+++ app/routes.py
@@ -1,42 +1,64 @@
-from flask import Blueprint, jsonify
-
-class Planet:
-    def __init__(self, id, name, description, color):
-        self.id=id
-        self.name=name
-        self.description=description
-        self.color=color
-
-planets=[
-    Planet(1, "Sun", "center of the solar system", "orange"),
-    Planet(2, "Saturn", "takes approx 29.5 years to complete its orbit", "yellow"),
-    Planet(3, "Mercury", "rules communication", "light grey")
-]
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, make_response, request
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@planets_bp.route("", methods=["GET"])
-def handle_planets():
-    planets_response = []
-    for planet in planets:
-        planets_response.append(
-            {
-                "id":planet.id,
-                "name":planet.name,
-                "description":planet.description,
-                "color":planet.color
-            }
-        )
-    return jsonify(planets_response)
+# Create a planet
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
 
-@planets_bp.route("/<planet_id>", methods=["GET"])
-def handle_planet(planet_id):
-    planet_id=int(planet_id)
-    for planet in planets:
-        if planet.id == planet_id:
-            return {
-                "id":planet.id,
-                "name":planet.name,
-                "description":planet.description,
-                "color":planet.color
-            },200
+    if "name" not in request_body or "description" not in request_body or "color" not in request_body:
+        return make_response("Invalid request", 400)
+
+    new_planet = Planet(
+        name=request_body["name"],
+        description=request_body["description"],
+        color=request_body["color"]
+    )
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return f"Planet {new_planet.name} created", 201
+
+
+# @planets_bp.route("", methods=["GET"])
+# def handle_planets():
+#     planets_response = []
+#     for planet in planets:
+#         planets_response.append(
+#             {
+#                 "id":planet.id,
+#                 "name":planet.name,
+#                 "description":planet.description,
+#                 "color":planet.color
+#             }
+#         )
+#     return jsonify(planets_response)
+
+# @planets_bp.route("/<planet_id>", methods=["GET"])
+# def handle_planet(planet_id):
+#     planet_id=int(planet_id)
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return {
+#                 "id":planet.id,
+#                 "name":planet.name,
+#                 "description":planet.description,
+#                 "color":planet.color
+#             },200
+
+# class Planet:
+#     def __init__(self, id, name, description, color):
+#         self.id=id
+#         self.name=name
+#         self.description=description
+#         self.color=color
+
+# planets=[
+#     Planet(1, "Sun", "center of the solar system", "orange"),
+#     Planet(2, "Saturn", "takes approx 29.5 years to complete its orbit", "yellow"),
+#     Planet(3, "Mercury", "rules communication", "light grey")
+# ]
