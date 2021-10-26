61d4e96263484277c51c1472cca0159f7a8db48c
brew doctor <>
2021-10-26 22:40:21 +0400

    this is the completed project

--- app/models/planet.py
+++ app/models/planet.py
@@ -3,5 +3,5 @@ from app import db
 
 class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title = db.Column(db.String)
+    name = db.Column(db.String)
     description = db.Column(db.String)
--- app/routes.py
+++ app/routes.py
@@ -2,61 +2,56 @@ from app import db
 from app.models.planet import Planet
 from flask import request, Blueprint, make_response, jsonify
 
-planets_bp = Blueprint('planets',__name__, url_prefix="/planets")
-@planets_bp.route("", methods=["POST"])
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+
+@planets_bp.route("", methods=["GET","POST"])
 def handle_planets():
-    request_body = request.get_json()
-    new_planet = Planet(title=request_body["title"],
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description
+            })
+        return jsonify(planets_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
                     description=request_body["description"])
 
-    db.session.add(new_planet)
-    db.session.commit()
+        db.session.add(new_planet)
+        db.session.commit()
 
-    return make_response(f"Planet {new_planet.title} successfully created", 201)
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
 
-# class Planets:
-#     def __init__(self, id, name, description, attribute):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.add_attribute = attribute
+    
+@planets_bp.route("/<planet_id>", methods=["GET", "PUT", "DELETE"])
+def handle_planet(planet_id):
+    planet= Planet.query.get(planet_id)
+    if planet is None:
+        return make_response("", 404)
 
-# planets_list = [
+    if request.method == "GET":
+        return {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description
+    }
+    elif request.method == "PUT":
+        form_data = request.get_json()
 
-# Planets(1,"Saturn","Ring System: Yes", "AVG Temp = -140C"),
-# Planets(2,"Venus","Ring System: No", "AVG Temp = 464C"),
-# Planets(3,"Uranus","Ring System: Yes", "AVG Temp = -195C"),
-# Planets(4,"Earth","Ring System: Yes", "AVG Temp = 15C"),
-# Planets(5,"Mars","Ring System: No", "AVG Temp = -65C"),
-# Planets(6,"Jupiter","Ring System: Yes", "AVG Temp = -110C"),
-# Planets(7,"Neptune","Ring System: Yes", "AVG Temp = -200C")
+        planet.name = form_data["name"]
+        planet.description = form_data["description"]
 
-# ]
+        db.session.commit()
 
-
-
-# @planets_bp.route("", methods=["GET"])
-# def handle_planets():
-#     planets_response=[]
-#     for planet in planets_list:
-#         planets_response.append(
-#             {"id": planet.id,
-#             "name": planet.name,
-#             "description":planet.description,
-#             "additional_attribute":planet.add_attribute
-#         }
-#         )
-#     return jsonify(planets_response)
-
-# @planets_bp.route("/<planet_id>", methods=["GET"])
-# def handle_planet(planet_id):
-#     planet_id=int(planet_id)
-
-#     for planet in planets_list:
-#         if planet.id==planet_id:
-#             return{
-#             "id": planet.id,
-#             "name": planet.name,
-#             "description":planet.description,
-#             "additional_attribute":planet.add_attribute
-#             }
+        return make_response(f"Planet #{planet.id} changed successfully!")
+    elif request.method == "DELETE":
+        db.session.delete(planet)
+        db.session.commit()
+        return make_response(f"Planet #{planet.id} deleted successfully!")
+    
