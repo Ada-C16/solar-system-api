d0bbac482c91e46ed516db37b71980a555a67573
Jessica <jessica.w.roberts@gmail.com>
2021-10-26 11:07:18 -0700

    Add GET planets

--- app/routes.py
+++ app/routes.py
@@ -4,26 +4,25 @@ from app.models.planet import Planet
 
 planet_bp = Blueprint('planets', __name__, url_prefix='/planets')
 
-@planet_bp.route('', methods=['POST'])
+@planet_bp.route('', methods=['POST', 'GET'])
 def handle_planets():
-    request_body = request.get_json()
-    planet_to_add = Planet(
-        name=request_body['name'],
-        description=request_body['description'],
-        color=request_body['color'])
-    
-    db.session.add(planet_to_add)
-    db.session.commit()
+    if request.method == 'POST':
+        request_body = request.get_json()
+        planet_to_add = Planet(
+            name=request_body['name'],
+            description=request_body['description'],
+            color=request_body['color'])
+        
+        db.session.add(planet_to_add)
+        db.session.commit()
 
-    return make_response(f'Planet {planet_to_add.name} successfully created', 201)
-
-# @planet_bp.route('', methods=['GET'])
-# def handle_planets():
-#     planets_response = list()
-
-#     for planet in planets:
-#         planets_response.append(planet.to_dict())
-#     return jsonify(planets_response)
+        return make_response(f'Planet {planet_to_add.name} successfully created', 201)
+    elif request.method == 'GET':
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append(planet.to_dict())
+        return jsonify(planets_response)
 
 # @planet_bp.route('/<id>', methods=['GET'])
 # def handle_single_planet(id):
