f434a4a89a3a4c65ec5dd2600212e8eea7f296a5
Lizet Tovar <lizett503@gmail.com>
2021-10-26 12:04:10 -0700

    Add PUT and DELETE methods

--- app/routes.py
+++ app/routes.py
@@ -30,15 +30,41 @@ def read_planets():
             )
         return jsonify(planets_response)
 
-@planets_bp.route("/<planet_id>", methods=["GET"])
+@planets_bp.route("/<planet_id>", methods=["GET", "PUT", "DELETE"])
 def read_planet(planet_id):
-    try:
-        planet_id = int(planet_id)
-    except:
-        return {"error": "planet_id must be an int"}, 400
-
     planet = Planet.query.get(planet_id)
-    return planet.to_json()
+    if planet is None:
+        return make_response ("This planet does not exist"), 404
+    request_body = request.get_json()
+
+    if request.method == "GET":
+        return planet.to_json()
+
+    elif request.method == "PUT":
+        planet.name = request_body["name"]
+        planet.description = request_body["description"]
+
+        db.session.commit()
+
+        return make_response(f"Planet #{planet.id} successfully updated")
+
+    elif "name" not in request_body or "description" not in request_body:
+        return {
+            "message": "Request requires both a name and description"
+        }, 400
+
+    elif request.method == "DELETE":
+        db.session.delete(planet)
+        db.session.commit()
+        return make_response(f"Planet #{planet.id} successfully deleted"), 200
+
+    # try:
+    #     planet_id = int(planet_id)
+    # except:
+    #     return {"error": "planet_id must be an int"}, 400
+
+    # planet = Planet.query.get(planet_id)
+    # return planet.to_json()
 
     # planet_response = []
     # for planet in planets:
