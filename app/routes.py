1f2efa13bfaf653c4c13c335dbdbaaad746c3453
Lux Barker <lux.barker13@gmail.com>
2021-10-26 12:01:48 -0700

    refactor handle_planet into separate functions

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import db
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, jsonify, make_response, request, abort
 from app.models.planet import Planet
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
@@ -27,27 +27,37 @@ def handle_planets():
         return jsonify(planets_response)
 
 
-@planets_bp.route("<id>", methods=["GET", "PATCH", "DELETE"])
-def handle_planet(id):
+def get_planet_from_id(id):
     try:
-        planet = Planet.query.get(id)
+        id = int(id)
     except:
-        return make_response("Invalid ID", 400)
-    if request.method == "GET":
-        return planet.to_dict()
-    
-    elif request.method == "PATCH":
-        request_body = request.get_json()
-        if "name" in request_body:
-            planet.name = request_body["name"]   
-        if "description" in request_body:
-            planet.description = request_body["description"]     
-        if "moons" in request_body:
-            planet.moons = request_body["moons"]
-        db.session.commit()
-        return jsonify([planet.to_dict(), "Update Successful"])
-    elif request.method == "DELETE":
-        db.session.delete(planet)
-        db.session.commit()
-        return make_response("Delete successful", 200)
+        abort(400, {"error": "invalid id"})
+    return Planet.query.get_or_404(id)
 
+
+@planets_bp.route("<id>", methods=["GET"])
+def read_planet(id):
+    planet = get_planet_from_id(id)
+    return planet.to_dict()
+
+
+@planets_bp.route("<id>", methods=["PATCH"])
+def update_planet(id):
+    planet = get_planet_from_id(id)
+    request_body = request.get_json()
+    if "name" in request_body:
+        planet.name = request_body["name"]
+    if "description" in request_body:
+        planet.description = request_body["description"]
+    if "moons" in request_body:
+        planet.moons = request_body["moons"]
+    db.session.commit()
+    return jsonify([planet.to_dict(), "Update Successful"])
+
+
+@planets_bp.route("<id>", methods=["DELETE"])
+def delete_planet(id):
+    planet = get_planet_from_id(id)
+    db.session.delete(planet)
+    db.session.commit()
+    return make_response("Delete successful", 200)
