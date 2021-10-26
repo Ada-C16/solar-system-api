b0b623577b7aa83955b7dc35e342bbb97ad6b44e
sarahstandish <z.sisyphusrocks@gmail.com>
2021-10-26 10:54:00 -0700

    resolving changes

--- app/routes.py
+++ app/routes.py
@@ -13,17 +13,6 @@ def handle_planets():
         if type(request_body) == list:
             for planet in request_body:
                 new_planet = Planet(name = planet["name"], description = planet["description"], has_moons = planet["has_moons"])
-<<<<<<< HEAD
-                db.session.add(new_planet)
-                db.session.commit()
-        else:
-            new_planet = Planet(name = request_body["name"], description = request_body["description"], has_moons = request_body["has_moons"])
-            if "name" not in request_body or "description" not in request_body or "has_moons" not in request_body:
-                return jsonify({"message": "Missing information - you need name, description, and if the planet has moons."}), 400
-
-            db.session.add(new_planet)
-            db.session.commit()
-=======
 
                 db.session.add(new_planet)
                 db.session.commit()
@@ -36,7 +25,6 @@ def handle_planets():
             db.session.commit()
 
         return f"New Planets Added!", 201
->>>>>>> f598d140d57c58f8de4d3e5095b343a466a62ea6
 
     elif request.method == "GET":
         planets = Planet.query.all()
@@ -45,30 +33,6 @@ def handle_planets():
             planet_list.append(planet.create_planet_dictionary())
         
         return jsonify(planet_list), 200
-<<<<<<< HEAD
-# Able to take both Get and Post requests
-# If POST: set up a variable to hold request body 
-# Use variable to create new planet instance
-# Add to .db session
-# Comit db session
-# Return a response with a 201 status code
-# If GET: Query database, get all planets
-# Create planets as dictionaries, put in a list
-# Return jsonify list
-
-# list_of_planets = []
-# for planet in planets:
-#     list_of_planets.append(planet.create_planet_dictionary()), 200
-
-# return jsonify(list_of_planets)
-
-@planets_bp.route("/<planet_id>", methods=["GET"])
-def handle_planet(planet_id):
-    pass
-    # for planet in planets:
-    #     if int(planet_id) == planet.id:
-    #         return jsonify(planet.create_planet_dictionary())
-=======
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def handle_planet(planet_id):
@@ -80,4 +44,3 @@ def handle_planet(planet_id):
         return jsonify(planet.create_planet_dictionary())
 
     return { "Error" : f"Planet {planet_id} was not found."}, 404
->>>>>>> f598d140d57c58f8de4d3e5095b343a466a62ea6
