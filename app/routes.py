a3fea77452449c1cdc9f4dae4f1f4e9e7958dccc
Rae <rae.elwell@gmail.com>
2021-10-26 11:51:50 -0700

    cleans up code

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,6 @@ from app import db
 from flask import Blueprint, jsonify, make_response, request
 from app.models.planet import Planet
 
-# planets = [Planet(1, "Mercury", "small and red", "solid"),     
-# Planet(5, "Jupiter", "big and swirly", "gaseous"), 
-# Planet(6, "Saturn", "rings and swirls", "gaseous")]
-
-
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 @planets_bp.route("", methods=["GET"])
