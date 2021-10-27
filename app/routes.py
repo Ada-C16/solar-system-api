774b2b1b0c576d4567e670dfaa3cd2e76dfacf2b
Kbhlee2121 <Kbhlee2121@gmail.com>
2021-10-27 11:07:11 -0700

    Refactor read_all_planets and create_planet

--- app/routes.py
+++ app/routes.py
@@ -4,29 +4,7 @@ from app.models.planet import Planet
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-
-@planets_bp.route("", methods=["POST", "GET"])
-def handle_planets():
-    if request.method == "POST":
-        request_body = request.get_json()
-        new_planet = Planet(
-            name=request_body["name"],
-            description=request_body["description"],
-            moons=request_body["moons"],
-        )
-
-        db.session.add(new_planet)
-        db.session.commit()
-
-        return make_response(f"New planet {new_planet.name} successfully created!", 201)
-    elif request.method == "GET":
-        planets = Planet.query.all()
-        planets_response = []
-        for planet in planets:
-            planets_response.append(planet.to_dict())
-        return jsonify(planets_response)
-
-
+# Helper Function
 def get_planet_from_id(id):
     try:
         id = int(id)
@@ -34,7 +12,29 @@ def get_planet_from_id(id):
         abort(400, {"error": "invalid id"})
     return Planet.query.get_or_404(id)
 
+# Routes
+@planets_bp.route("", methods=["GET"])
+def read_all_planets():
+    planets = Planet.query.all()
+    planets_response = []
+    for planet in planets:
+        planets_response.append(planet.to_dict())
+    return jsonify(planets_response)
 
+@planets_bp.route("", methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+    new_planet = Planet(
+        name=request_body["name"],
+        description=request_body["description"],
+        moons=request_body["moons"],
+    )
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"New planet {new_planet.name} successfully created!", 201)
+    
 @planets_bp.route("<id>", methods=["GET"])
 def read_planet(id):
     planet = get_planet_from_id(id)
