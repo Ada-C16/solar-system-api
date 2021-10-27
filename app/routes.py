055f1a044ef453f4b868e74761e95bb16e82aaa2
Maria Obregon Garcia <mobregong@gmail.com>
2021-10-27 14:55:04 -0400

    Refactor routes to have single method

--- app/routes.py
+++ app/routes.py
@@ -4,54 +4,65 @@ from flask import Blueprint, jsonify, request, make_response, abort
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@planets_bp.route("", methods=["POST", "GET"], strict_slashes=False)
-def handle_planets():
-    if request.method == "POST":
-        request_body = request.get_json()
-        new_planet = Planet(
-                    name=request_body["name"],
-                    description=request_body["description"],
-                    size_rank=request_body["size_rank"]
-                    )
-        db.session.add(new_planet)
-        db.session.commit()
-
-        return make_response(f"Planet {new_planet.name} successfully created", 201)
-    
-    elif request.method == "GET":
-        planet_objects = Planet.query.all() # returning list of objects
-        response_list = []
-        for planet in planet_objects:
-            response_list.append(planet.to_dict())
-        return make_response(jsonify(response_list), 200)
-
-
-@planets_bp.route("/<planet_id>", methods=["GET", "PATCH", "DELETE"],)
-def handle_single_planet(planet_id):
+#Helper functions
+def valid_planet(planet_id):
     try: 
-        planet_id = int(planet_id)
+        int(planet_id)
     except:
         abort(make_response({"error": "planet_id must be an int"}, 400))
-    
+
+def get_planet_from_id(planet_id):
+    valid_planet(planet_id)
     selected_planet = Planet.query.get_or_404(planet_id)
-    
-    if request.method == "GET":
-        return selected_planet.to_dict()
+    return selected_planet
 
-    elif request.method == "PATCH":
-        request_body = request.get_json()
-        if "name" in request_body:
-            selected_planet.name = request_body["name"]
-        if "description" in request_body:
-            selected_planet.description = request_body["description"]
-        if "size_rank" in request_body:
-            selected_planet.size_rank = request_body["size_rank"]
-        db.session.commit()
-        return make_response(f"{selected_planet.name} updated", 200)
+# Routes
+@planets_bp.route("", methods=["POST"], strict_slashes=False)
+# def handle_planets():
+def add_planet():
+    request_body = request.get_json()
+    new_planet = Planet(
+        name=request_body["name"],
+        description=request_body["description"],
+        size_rank=request_body["size_rank"]
+        )
+    db.session.add(new_planet)
+    db.session.commit()
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
 
-    elif request.method == "DELETE":
-        db.session.delete(selected_planet)
-        db.session.commit()
-        return make_response(f"{selected_planet.name} deleted", 200)
+@planets_bp.route("", methods=["GET"], strict_slashes=False)
+def get_all_planets():
+    planet_objects = Planet.query.all() # returning list of objects
+    response_list = []
+    for planet in planet_objects:
+        response_list.append(planet.to_dict())
+    return make_response(jsonify(response_list), 200)
+
+# @planets_bp.route("/<planet_id>", methods=["GET", "PATCH", "DELETE"],)
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_planet(planet_id):
+    selected_planet = get_planet_from_id(planet_id)
+    return selected_planet.to_dict()
+
+@planets_bp.route("/<planet_id>", methods=["PATCH"])
+def update_planet(planet_id):
+    selected_planet = get_planet_from_id(planet_id)
+    request_body = request.get_json()
+    if "name" in request_body:
+        selected_planet.name = request_body["name"]
+    if "description" in request_body:
+        selected_planet.description = request_body["description"]
+    if "size_rank" in request_body:
+        selected_planet.size_rank = request_body["size_rank"]
+    db.session.commit()
+    return make_response(f"{selected_planet.name} updated", 200)
+
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def delete_planet(planet_id):
+    selected_planet = get_planet_from_id(planet_id)
+    db.session.delete(selected_planet)
+    db.session.commit()
+    return make_response(f"{selected_planet.name} deleted", 200)
         
 
