8575a0580d15a615cb46d682d394c3494cd4efd5
sjolivas <sjolivas@gmail.com>
2021-10-27 13:10:00 -0500

    Set up configuration for API testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,27 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
-    app.config['SQLALCHEMY_ECHO'] = True
+    
+    if not test_config:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+        app.config['SQLALCHEMY_ECHO'] = True
+
+    else:
+        app.config["TESTING"] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+        app.config['SQLALCHEMY_ECHO'] = True
+
     # import models here
     from app.models.planet import Planet
 
