727eabff713db308a84eb3011adc0a14f883dcd9
Veronica Osei <veronicaosei273@gmail.com>
2021-10-27 13:05:24 -0500

    create .env file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    from .routes import planet_system_bp
-    app.register_blueprint(planet_system_bp)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    from app.models.solar import Planet
+    db.init_app(app)
+    migrate.init_app(app,db)
+
+
+    # from .routes import planet_system_bp
+    # app.register_blueprint(planet_system_bp)
 
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
--- app/routes.py
+++ app/routes.py
@@ -1,40 +1,74 @@
-from flask import Blueprint,jsonify
+from app import db
+from app.models.solar import Planet
+from flask import Blueprint,jsonify, make_response, request
 
-class Planet:
-    def __init__(self,id,name,description):
-        self.id = id
-        self.name = name
-        self.description = description
 
-planets = [
-    Planet(1, "Mercury", "Very windy."),
-    Planet(2, "Venus", "Brightest planet."),
-    Planet(3, "Saturn", "Icy rings.")
-]
-
-planet_system_bp = Blueprint("planet_system", __name__)
 planets_bp = Blueprint("planets_py", __name__, url_prefix= "/planets")
 
-@planets_bp.route("/<planet_id>", methods=["GET"])
-def handle_planets(planet_id):
+@planets_bp.route("", methods=["GET","POST"])
+def handle_planets():
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append(
+                {
+                    "id": planet.id,
+                    "name": planet.name,
+                    "description": planet.description
+                })
+        return make_response(f"All the planet {planets_response}" , 201)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
+            description=request_body["description"])
 
-    for planet in planets:
-        if planet.id == int(planet_id):
-            return {
-                "id" : planet.id,
-                "name" : planet.name,
-                "description" : planet.description
-            }
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
+ 
+@planets_bp.route("/<planet_id>", methods=["GET", "PUT", "DELETE"])
+def handle_planet(planet_id):
+    # planet_id = int(planet_id)
+    planet = Planet.query.get(planet_id)
+
+    if planet == None:
+        return make_response("", 404)
+
+    if request.method == "GET":
+        return {
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description
+        }
+
+    elif request.method == "PUT":
+        request_body = request.get_json()
+
+        planet.name = request_body["name"]
+        planet.description = request_body["description"]
+
+        db.session.commit()
+
+        return make_response(f"Planet #{planet.id} successfully updated")
+    elif request.method == "DELETE":
+        db.session.delete(planet)
+        db.session.commit()
+        return make_response(f"Planet #{planet.id} successfully deleted")
 
 
-@planet_system_bp.route("/planets", methods= ['GET'])
-def get_planet_system_json():
-    return {
-        "planet_1" : "Mercury",
-        "planet_2" : "Venus",
-        "planet_3" : "Saturn"
-    }
 
 
-
-
+   # elif request.method == "GET":
+    #     planets = Planet.query.all()
+    #     planets_response = []
+    #     for planet in planets:
+    #         planets_response.append(
+    #             {
+    #                 "id": planet.id,
+    #                 "name": planet.name,
+    #                 "description": planet.description
+    #             }
+    #         )
+    #     return jsonify(planets_response)
