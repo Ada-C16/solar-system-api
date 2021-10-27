778f1eff9247bff181b3244d73e2a2eb868f404a
jacy <jacyyang04@gmail.com>
2021-10-27 11:29:41 -0700

    refactor all functions

--- app/routes.py
+++ app/routes.py
@@ -15,65 +15,68 @@ def validate_int(user_input, attribute_name):
     except:
         abort(make_response({"error": "{attribute_name} must be an int"}, 404))
 
+def get_planet(planet_id):
+    validate_int(planet_id, "id")
+
+    return Planet.query.get_or_404(planet_id, description="Planet does not exist.")
+
 # Routes
-@planets_bp.route("", methods=["GET", "POST"])
-def handle_planets():
+@planets_bp.route("", methods=["POST"])
+def create_planets():
     request_body = request.get_json()
 
-    if request.method == "POST":
-        if "name" not in request_body:
-            return make_response("Invalid Request", 404)
+    if request_body is None:
+        return make_response("Invalid Request", 404)
 
-        new_planet = Planet(
-            name=request_body['name'],
-            # description=request_body['description'],
-            # xenomorphs=request_body['']
-        )
+    new_planet = Planet(
+        name=request_body['name'],
+        description=request_body['description'],
+        xenomorphs=request_body['xenomorphs']
+    )
 
-        db.session.add(new_planet)  # like git, stagging changes
-        db.session.commit()  # committing to database
+    db.session.add(new_planet)  # like git, stagging changes
+    db.session.commit()  # committing to database
 
-        return make_response(f"Your planet, {new_planet.name}, has been created.", 201)
+    return make_response(f"Your planet, {new_planet.name}, has been created.", 201)
 
-    elif request.method == "GET":
-        planets = Planet.query.all()
-        planets_response = []
+@planets_bp.route("", methods=["GET"])
+def handle_planets():
+    planets = Planet.query.all()
+    planets_response = []
 
-        for planet in planets:
-            planets_response.append(planet.to_json())
+    for planet in planets:
+        planets_response.append(planet.to_json())
 
-        return jsonify(planets_response)
+    return jsonify(planets_response)
 
 
-@planets_bp.route("/<planet_id>", methods=["GET", "PATCH", "DELETE"])
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_planet(planet_id):
+    planet = get_planet(planet_id)
+    return jsonify(planet.to_json(), 200)
+
+@planets_bp.route("/<planet_id>", methods=["PATCH"])
 def get_planet(planet_id):
     request_body = request.get_json()
-    planet = Planet.query.get(planet_id)
+    planet = get_planet(planet_id)
 
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(make_response({"error": "planet_id must be an int"}, 400))
+    if "id" in request_body:
+        planet.id = request_body["id"]
+    if "name" in request_body:
+        planet.name = request_body["name"]
+    if "description" in request_body:
+        planet.description = request_body["description"]
+    if "xenomorphs" in request_body:
+        planet.xenomorphs = request_body["xenomorphs"]
 
-    if request.method == 'GET':
-        if planet == None:
-            return make_response("your planet ain't real.", 404)
-        return planet.to_json()
+    db.session.commit()
+    return jsonify(planet.to_json(), 201)
 
-    elif request.method == 'PATCH':
-        if "id" in request_body:
-            planet.id = request_body["id"]
-        if "name" in request_body:
-            planet.name = request_body["name"]
-        if "description" in request_body:
-            planet.description = request_body["description"]
-        if "xenomorphs" in request_body:
-            planet.xenomorphs = request_body["xenomorphs"]
 
-        db.session.commit()
-        return jsonify(planet.to_json(), 201)
-        
-    elif request.method == 'DELETE':
-        db.session.delete(planet)
-        db.session.commit()
-        return make_response(f"Planet #{planet_id} successfully destroyed.", 200)
+@planets_bp.route("/<planet_id>", methods=["DELETE"])
+def get_planet(planet_id):
+    planet = get_planet(planet_id)      
+
+    db.session.delete(planet)
+    db.session.commit()
+    return make_response(f"Planet #{planet_id} successfully destroyed.", 200)
