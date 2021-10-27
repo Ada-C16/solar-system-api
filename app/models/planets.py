0592a4e510950acdba0df5f47641a73cc9ec0cbe
Mariah Rosman <mrosman7@gmail.com>
2021-10-27 12:09:14 -0700

    adds to_dict() function to Planet Model

--- app/models/planets.py
+++ app/models/planets.py
@@ -6,3 +6,11 @@ class Planet(db.Model):
     name = db.Column(db.String(64))
     description = db.Column(db.String(64))
     num_moons = db.Column(db.Integer)
+
+    def to_dict(self):
+        return ({
+            "id" : self.id,
+            "name" : self.name,
+            "description" : self.description,
+            "num_moons" : self.num_moons
+            })
