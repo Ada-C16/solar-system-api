725d30c375b4d198db2ba7da4352b95bba6d6f1e
Arianna Cabebe <aricabebe@gmail.com>
2021-10-28 15:18:25 -0700

    added all tests. all tests passing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from app import create_app
 from app import db
+from app.models.planet import Planet
 
 @pytest.fixture
 def app():
@@ -12,7 +13,21 @@ def app():
     with app.app_context():
         db.drop_all()
 
-
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def add_planet():
+    pluto = Planet(name="pluto", description="way out there", sign="Scorpio")
+    db.session.add(pluto)
+    db.session.commit()
+
+@pytest.fixture
+def add_three_planets():
+    pluto = Planet(name="pluto", description="way out there", sign="Scorpio")
+    mercury = Planet(name="mercury", description="messenger closest to sign", sign="Virgo")
+    saturn = Planet(name="saturn", description="forever hula hooping", sign="Capricorn")
+
+    db.session.add_all([pluto, mercury, saturn])
+    db.session.commit()
