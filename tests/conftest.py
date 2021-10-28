35c5d01a8de5dfe0b8e24fabc5df0f20dfb9dfe2
Lux Barker <lux.barker13@gmail.com>
2021-10-28 11:38:57 -0700

    test get one planet by id

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
 import pytest
 from app import create_app, db
+from app.models.planet import Planet
 # ************************
 # app - simulate flask run
 # ************************
+
+
 @pytest.fixture
 # name this app because it is the simulation of the app (the flask server)
 def app():
@@ -11,7 +14,7 @@ def app():
     # and is not empty
     # but you can set up more complicated logic in __init__.py if needed
     app = create_app({"Testing": True})
-    
+
     # SETTING UP
     # the next steps are going to be in the context of the simulated test app
     with app.app_context():
@@ -20,18 +23,20 @@ def app():
         # when we do flask db init, migrate and update in terminal
         db.create_all()
         # pause this function for now until after tests are run
-        yield app # this line divides set up and clean up!
-        
+        yield app  # this line divides set up and clean up!
+
     # CLEANING UP (or tearing down)
     # after tests are finished running
     with app.app_context():
         # drop all the db tables and clear all data
         # so that you can have a fresh db every time
         db.drop_all()
-        
-# **************************    
+
+# **************************
 # client - simulate requests
 # **************************
+
+
 @pytest.fixture
 # the parameter app is referring to the app fixture right above it
 # pytest knows to inject the app in when running this func
@@ -42,3 +47,10 @@ def client(app):
 # ****************************
 # data to be populated into db
 # ****************************
+
+
+@pytest.fixture
+def one_saved_planet(app):
+    earth = Planet(name="earth", description="blue marble", moons=1)
+    db.session.add(earth)
+    db.session.commit()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -5,3 +5,18 @@ def test_get_all_planets_returns_empty_list_when_db_is_empty(client):
     # Assert
     assert response.status_code == 200
     assert response.get_json() == []
+
+
+def test_get_one_planet_by_id_returns_correct_planet(client, one_saved_planet):
+    # Act
+    response = client.get("/planets/1")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == {
+        "name": "earth",
+        "description": "blue marble",
+        "moons": 1,
+        "id": 1
+    }
