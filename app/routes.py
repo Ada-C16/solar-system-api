c2e4e1bd0156eb0f0b6830aca286f010ccaa966a
mac-madison <macanmadison@gmail.com>
2021-10-28 12:57:46 -0700

    Refactored handle_planet function

--- app/routes.py
+++ app/routes.py
@@ -23,3 +23,10 @@ def handle_planets():
         db.session.commit()
 
         return make_response(f"Planet {new_planet.name} successfully created", 201)
+
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+
+    return planet.to_json()
