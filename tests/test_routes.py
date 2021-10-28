2e2dd49c195049ae75f250f28de01a993a141118
bailey <baileyholderman@gmail.com>
2021-10-28 11:46:43 -0700

    first test for empty db

---
+++ tests/test_routes.py
@@ -0,0 +1,7 @@
+# test that app returns empty list when db is empty
+def test_get_all_planets_with_no_records(client):
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == []
