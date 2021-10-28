d4c67755a9722a3b60fbeb0493376348e0118f30
goblinbrooke <BrookeVlahos@gmail.com>
2021-10-28 12:02:10 -0700

    All Tests Passed!! Final

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,3 +19,16 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def one_planet(app):
+    planet1 = Planet(name="Mercury", description="Closest to the sun, hot.", has_moons=0)
+    db.session.add(planet1)
+    db.session.commit()
+
+@pytest.fixture
+def two_planets(app):
+    planet1 = Planet(name="Mercury", description="Closest to the sun, hot.", has_moons=0)
+    planet2 = Planet(name="Venus", description="Spins the opposite direction of other planets.", has_moons=0)
+    db.session.add_all([planet1, planet2])
+    db.session.commit()
