221a4e3a4c019c0538e3d3c8fb79b29a7d31ff04
Melinda <melindahayes84@gmail.com>
2021-10-28 12:22:22 -0700

    create tetst for handle planets and passes

---
+++ tests/test_routes.py
@@ -0,0 +1,6 @@
+def test_handle_planets_returns_200_and_empty_array(client):
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == []
