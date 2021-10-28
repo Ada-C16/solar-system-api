8ebb419ccf7ebbea457c91ad78db8a1645c4e2ab
mac-madison <macanmadison@gmail.com>
2021-10-28 12:45:23 -0700

    handle_plants POST

--- app/models/planet.py
+++ app/models/planet.py
@@ -5,3 +5,10 @@ class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     description = db.Column(db.String)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,18 @@
-from flask import Blueprint, jsonify
-
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, make_response, request
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+
+@planets_bp.route("", methods=["POST"])
+def handle_planets():
+    request_body = request.get_json()
+    new_planet = Planet(
+        name=request_body["name"], description=request_body["description"]
+    )
+
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created", 201)
