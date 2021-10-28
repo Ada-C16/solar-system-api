41566c4622c43db4c830697c67c8b9d3e0897bf7
letypl12 <86847701+letypl12@users.noreply.github.com>
2021-10-28 12:00:31 -0700

    All done

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from app import create_app, db
+from app.models.Planet import Planet
 
 @pytest.fixture
 def app():
@@ -14,4 +15,13 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def two_planets(app):
+    planet_1=Planet(name="Pluto", description="The unlucky one", biggest_moon="")
+    planet_2=Planet(name="Saturn", description="The one with rings", biggest_moon="Titan")
+    db.session.add_all([planet_1, planet_2])
+    db.session.commit()
+
+
 
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -7,4 +7,41 @@ def test_get_all_planets_with_no_records(client):
     assert response.status_code==200
     assert response_body==[]
 
-    
+def test_get_one_planet_two_planets_db(client, two_planets):
+    #Act
+    response=client.get("/planets/1")
+    response_body=response.get_json()
+
+    #Assert
+    assert response.status_code==200
+    assert response_body=={"name":"Pluto", "description":"The unlucky one", "biggest_moon":"", "id":1}
+
+def test_get_one_planet_with_no_records(client):
+    #Act
+    response=client.get("/planets/1")
+    response_body=response.get_json()
+
+    #Assert
+    assert response.status_code==404
+    assert response_body== None
+
+def test_get_all_planets_two_planets_db(client, two_planets):
+    #Act
+    response=client.get("/planets")
+    response_body=response.get_json()
+
+    #Assert
+    assert response.status_code==200
+    assert len(response_body)==2
+
+def test_post_planet(client):
+    #Arrange
+    planet={"name":"Pluto", "description":"The unlucky one", "biggest_moon":""}
+    #Act
+    response=client.post("/planets", json=planet)
+    response_body=response.get_json()
+    #Assert
+    assert response.status_code==201
+    #assert response_body=="Pluto succesfully created"
+
+
