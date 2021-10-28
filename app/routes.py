cb1bf57e45ba6248cd351f0ce47a84199408251d
mac-madison <macanmadison@gmail.com>
2021-10-28 10:32:07 -0700

    Created Planet class & instances

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,28 @@
 from flask import Blueprint
 
-planets_bp = Blueprint("planets", __name__)
 
+class Planet:
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+
+
+planets = [
+    Planet(1, "Mercury", "Coffee"),
+    Planet(1, "Venus", "Girl"),
+    Planet(1, "Earth", "Us"),
+    Planet(1, "Mars", "Elon"),
+    Planet(1, "Jupiter", "Big"),
+    Planet(1, "Saturn", "Rings"),
+    Planet(1, "Uranus", "Lol"),
+    Planet(1, "Neptune", "Cold"),
+    Planet(1, "Pluto", "Who?"),
+]
+
+planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+
+@planets_bp.ropute("/planets", methods=["GET"])
+def handle_planets():
+    response_body = "Success"
