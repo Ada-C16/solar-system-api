e6861767d648dce270b67e5a5875e3f2c169f8b3
Kbhlee2121 <Kbhlee2121@gmail.com>
2021-10-28 11:49:50 -0700

    Add test for get all planets in a list

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -25,3 +25,15 @@ def test_get_one_planet_by_id_returns_correct_planet(client, one_saved_planet):
 def test_get_one_planet_by_id_with_empty_db_returns_404(client):
     response = client.get("/planets/1")
     assert response.status_code == 404
+
+def test_get_all_planets_returns_list_of_planets(client, one_saved_planet):
+    response = client.get("/planets")
+    assert response.status_code == 200
+    assert response.get_json() == [ 
+        {
+        "name": "earth",
+        "description": "blue marble",
+        "moons": 1,
+        "id": 1
+        }
+    ]
