19ece102308afe029d4119e179db018f62c38e1f
jacy <jacyyang04@gmail.com>
2021-10-28 11:57:39 -0700

    add test routes

---
+++ tests/conftest.py
@@ -0,0 +1,38 @@
+# file to store all my fixtures; confiure test: conftest
+
+import pytest
+from app import create_app
+from app import db
+from app.models.planet import Planet
+
+# app
+@pytest
+def app():
+    app = create_app({"TESTING": True})
+
+    with app.app_context():
+        db.create_all()  # creates empty database
+        yield app
+
+    with app.app_context():
+        db.drop_all()   # deletes data
+
+# client
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+# data
+@pytest.fixture
+def save_two_planets(app):
+    # Arrange
+    dune_planet = Planet(name="Arrakis",
+                        description="sand",
+                        xenomorhps=False)
+
+    water_planet = Planet(title="Water",
+                        description="waterisLife",
+                        xenomorphs=True)
+
+    db.session.add_all([dune_planet, water_planet])
+    db.session.commit()
