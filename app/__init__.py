eebc13c0a1eaabb5d5068708a212dbd75b103c55
Afina <afinawalton95@gmail.com>
2021-11-03 11:24:45 -0700

    Add .env and update DEV_DB info

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv, main
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DEV_DB")
 
+    from app.models.planet import Planet
     db.init_app(app)
     migrate.init_app(app, db)
 
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
 
-    from app.models.planet import Planet
     return app
