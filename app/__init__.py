aa9e5bbfd87d29e8756b031860cadf1a9228d573
Khandice <s.khandice@gmail.com>
2021-11-03 11:29:39 -0700

    Updates path of uri to .env file

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
 
 db = SQLAlchemy()
 migrate = Migrate()
-DATABASE = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+load_dotenv()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+    if test_config is None:
+            app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+                "SQLALCHEMY_DATABASE_URI")
+    else:
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+            "SQLALCHEMY_TEST_DATABASE_URI")
 
     db.init_app(app)
     migrate.init_app(app,db)
