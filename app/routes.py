1cb13c0b5d794bf8187a41bd0ef4aff219740c46
Aisha <ishaarigbabu@gmail.com>
2021-11-20 16:53:01 -0600

    adds search with planet name

--- app/routes.py
+++ app/routes.py
@@ -3,21 +3,6 @@ from app import db
 from app.models.planets import Planet
 
 
-# class Planet:
-#     def __init__(self, id, name, description, oxygen):
-#         self.id = id
-#         self.name = name
-#         self.description = description
-#         self.oxygen = oxygen
-
-
-# planet1 = Planet(1, "mecury", "its a pretty planet", 0.2)
-# planet2 = Planet(2, "sun", "its friggin hot", 0)
-# planet3 = Planet(3, "moon", "its pretty at night", 2)
-
-
-# list_of_planets = [planet1, planet2, planet3]
-
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 @planets_bp.route("", methods = ["GET", "POST"])
@@ -26,7 +11,15 @@ def handle_planets():
 
     if request.method == "GET":
         planet_response = []
-        list_of_planets = Planet.query.all()
+
+        #name_query_param = request.args.get("name")
+        name_query_param = request.args.get("name")
+
+        if name_query_param is not None:
+            list_of_planets = Planet.query.filter_by(name = name_query_param)
+            
+        else:
+            list_of_planets = Planet.query.all()
         for planet in list_of_planets:
             planet_response.append(
                 {
@@ -54,21 +47,52 @@ def handle_planets():
         return make_response(
             f"Planet {new_planet.name} created", 200
         )
+        # planet.query.filter_by(param) # use to filter query results
+        # planet.query.limit(100).all() limit number of results
 #@planets_bp.route("/<planet_name>", methods = ["GET"])
-@planets_bp.route("/<planet_id>", methods = ["GET"])
-def get_planet_by_name(planet_id):
+@planets_bp.route("/<planet_id>", methods = ["GET", "PUT", "DELETE"])
+def get_planet_by_id(planet_id):
     planet = Planet.query.get(planet_id)
     #for planet in list_of_planets:
     #if planet.title == planet_name:
-    try:
-        return {
-            "id" : planet.id,
-            "name": planet.name,
-            "description" : planet.description,
-            "oxygen_level" : planet.oxygen_level
-        }
-    except:
-        planet is None
-        return make_response(f"planet {planet_id} is not found", 404)
+    if request.method == "GET":
+        try:
+            return {
+                "id" : planet.id,
+                "name": planet.name,
+                "description" : planet.description,
+                "oxygen_level" : planet.oxygen_level
+            }
+        except:
+            planet is None
+            return make_response(f"planet {planet_id} is not found", 404)
+    
+    elif request.method == "PUT":
+        form_data = request.get_json()
+
+        if "name" not in form_data or "description" not in form_data or "oxygen_level" not in form_data:
+            return make_response(f"you must include a name, description and oxygen_level for your planet", 400)
+
+        try:
+            planet.name = form_data["name"]
+            planet.description = form_data["description"]
+            planet.oxygen_level = form_data["oxygen_level"]
+
+            return make_response(f"{planet.name} successfully updated", 200)
+        except:
+            planet is None
+            return make_response(f"planet {planet.id} does not exist")
+        
+    elif request.method == "DELETE":
+        planet_to_delete = planet
+        try:
+            db.session.delete(planet_to_delete)
+            db.session.commit()
+
+            return make_response(f"planet {planet.name} successfully deleted", 200)
+        except:
+            planet_to_delete is None
+            return make_response(f"planet {planet_id} does not exsist", 404)
+
 
 
